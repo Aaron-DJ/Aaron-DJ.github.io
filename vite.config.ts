a06652c2c549aeb3d4344eaf375788cf0f7909a1
Aaron-DJ <aaron.david.jacobsen@gmail.com>
2026-02-02 19:04:56 -0600

    Fix and add deploy script for pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-})
+  base: "/Aaron-DJ.github.io/",
+  plugins: [tailwindcss(), react()],
+});
